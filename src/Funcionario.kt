d086bc3af39ca7ab43a5fe4e4355b52699af2c67
Alexandre Oliveira <alexmdo@gmail.com>
2022-12-16 09:22:48 -0300

    Adicionando mais código no funcionário

--- src/Funcionario.kt
+++ src/Funcionario.kt
@@ -1,17 +1,39 @@
 class Funcionario(
     val nome: String,
     val cpf: String,
-    val salario: Double
+    val salario: Double,
+    val tipo: Int
 ) {
 
     fun calcularBonificacao(): Double {
-        return salario * 0.1
+        when (tipo) {
+            0 -> {
+                return salario * .1
+            }
+            1 -> {
+                return salario * .2
+            }
+            else -> {
+                return salario * .3
+            }
+        }
+    }
+
+    fun autentica(senha: Int) {
+        if (tipo == 1) {
+            // TODO
+        }
     }
 
 }
 
 fun main() {
-    val alexandre = Funcionario("Alexandre", "111.111.111-11", 1000.00)
+    val alexandre = Funcionario(
+        "Alexandre",
+        "111.111.111-11",
+        1000.00,
+        0
+    )
     println("nome do funcionario: ${alexandre.nome}")
     println("cpf do funcionario: ${alexandre.cpf}")
     println("salario do funcionario: ${alexandre.salario}")
